703b274931f97b21bce69cd1365093c9ae0ea4f9
John Haugeland <stonecypher@gmail.com>
2014-11-05 00:33:45 -0800

    adding deps, yuidoc, gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+global.errorMessage = '';
+
+var gulp      = require('gulp'),
+    clean     = require('gulp-clean'),
+    yuidoc    = require('gulp-yuidoc'),
+    sloc      = require('gulp-sloc');
+
+gulp.task('clean', function() {
+  return gulp.src(['./doc'], {read: false}).pipe(clean());
+});
+
+gulp.task('docs', ['clean'], function() {
+  gulp.src('./assets/js/**/*.js')  // todo wrong
+    .pipe(yuidoc())
+    .pipe(gulp.dest("./doc"));
+});
+
+gulp.task('vows', function() {
+  // todo whargarbl
+});
+
+gulp.task('build',  ['test','docs']);
+gulp.task('test',   ['vows']);
+
+gulp.task('sloc', ['build'], function(){
+  gulp.src(['gulpfile.js'])
+    .pipe(sloc());
+});
+
+gulp.task('default', ['sloc','test']);
