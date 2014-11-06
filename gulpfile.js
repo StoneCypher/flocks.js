d8d4f6ddce42081e5e898e0f757e5b35a881470d
John Haugeland <stonecypher@gmail.com>
2014-11-05 20:35:55 -0800

    and we now have gulp lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,41 @@
 
+/* jshint node: true */
+/* eslint-env node, browser */
+"use strict";
 
+var gulp            = require("gulp"),
+    shell           = require("gulp-shell"),
+    clean           = require("gulp-clean"),
+    yuidoc          = require("gulp-yuidoc"),
+    stripDomComment = require("gulp-strip-react-dom-comment"),
+    sloc            = require("gulp-sloc"),
+    linttask        = require("sc-eslint/estask.js");
 
-global.errorMessage = '';
+global.errorMessage = "";
 
-var gulp            = require('gulp'),
-    shell           = require('gulp-shell'),
-    clean           = require('gulp-clean'),
-    yuidoc          = require('gulp-yuidoc'),
-    stripDomComment = require('gulp-strip-react-dom-comment'),
-    sloc            = require('gulp-sloc'),
-    eslint_task     = require('sc-eslint/estask.js');
-
-gulp.task('clean', function() {
-  return gulp.src(['./doc'], {read: false}).pipe(clean());
+gulp.task("clean", function() {
+  return gulp.src(["./doc"], {"read" : false}).pipe(clean());
 });
 
-gulp.task('docs', ['clean'], function() {
-  gulp.src(['./lib/*.js', './lib/*.jsx'])  // todo wrong
+gulp.task("docs", ["clean"], function() {
+  gulp.src(["./lib/*.js", "./lib/*.jsx"])
     .pipe(stripDomComment())
     .pipe(yuidoc())
-    .pipe(gulp.dest('./doc'));
+    .pipe(gulp.dest("./doc"));
 });
 
-var lintconfig = { target: "gulpfile.js test/enforce-tests.js" };
-gulp.task('lint', eslint_task(lintconfig));
+gulp.task("lint", linttask({
+  "targets" : "gulpfile.js lib/flocks.js test/enforce-tests.js"
+}));
 
-gulp.task('vows', shell.task("vows test/* --spec -s"));
+gulp.task("vows", shell.task("vows test/* --spec -s"));
 
-gulp.task('build',  ['test','docs']);
-gulp.task('test',   ['vows', 'eslint']);
+gulp.task("build",  ["test", "docs"]);
+gulp.task("test",   ["vows", "lint"]);
 
-gulp.task('sloc', ['build'], function(){
-  gulp.src(['gulpfile.js', 'lib/*.js', 'lib/*.jsx', 'test/*.js'])
-    .pipe(sloc({tolerant: true}));
+gulp.task("sloc", ["build"], function() {
+  gulp.src(["gulpfile.js", "lib/*.js", "lib/*.jsx", "test/*.js"])
+    .pipe(sloc({"tolerant" : true}));
 });
 
-gulp.task('default', ['sloc','test']);
+gulp.task("default", ["sloc", "test"]);
