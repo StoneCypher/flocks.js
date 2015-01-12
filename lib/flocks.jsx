718179cfda183d6f8213ff08fe9f2ab6f5ad377d
John Haugeland <stonecypher@gmail.com>
2015-01-11 17:42:50 -0800

    Major: moving to second implementation, mild breaking changes

--- lib/flocks.jsx
+++ lib/flocks.jsx
@@ -11,7 +11,9 @@
 
 
 
-'use strict';
+
+// if it's in a <script> it's defined already
+// otherwise assume commonjs
 
 if (typeof React === 'undefined') {
     var React = require('react');
@@ -21,13 +23,112 @@ if (typeof React === 'undefined') {
 
 
 
-/**
- * @method clone
- * @for    Flocks
- * @return {Object} Deep memberwise clone of an object
- */
+// wrap the remainder
 
-var clone = function(obj) {
+(function() {
+
+
+
+
+
+    'use strict';
+
+
+
+
+
+    var initialized  = false,
+        updateBlocks = 0,
+        dirty        = false,
+
+        tagtype      = undefined,
+
+        handler      = function() { return true; },
+        finalizer    = function() { return true; },
+
+        prevFCtx     = {},
+        nextFCtx     = {},
+
+        flocks2_ctxs = { flocks2context: React.PropTypes.object };
+
+
+
+
+
+    function flocksLog(X) {
+//      console.log(X);  // later make this disableable, whargarbl
+    }
+
+
+
+
+
+    function enforceString(On, Label) {
+        Label = Label || 'Argument must be a string';
+        if (typeof On !== 'string') {
+            throw Label;
+        }
+    }
+
+
+
+
+
+    function isArray(maybeArray) {
+
+        return (Object.prototype.toString.call(maybeArray) === '[object Array]');
+
+    }
+
+
+
+
+
+    function isNonArrayObject(maybeArray) {
+
+        if (typeof maybeArray !== 'object')                                  { return false; }
+        if (Object.prototype.toString.call(maybeArray) === '[object Array]') { return false; }
+
+        return true;
+
+    }
+
+
+
+
+
+    function setByKey(Key, MaybeValue) {
+        enforceString(Key, "Flocks2 set/2 must take a string for its key");
+        nextFCtx[Key] = MaybeValue;
+        flocksLog("   - Flocks2 setByKey \"" + Key + "\"");
+        attemptUpdate();
+    }
+
+
+
+
+
+    function setByPath(Key, MaybeValue) { flocksLog('   - Flocks2 setByPath stub'  ); attemptUpdate(); }
+    function setByObject(Key, MaybeValue) { flocksLog('   - Flocks2 setByObject stub'); attemptUpdate(); }
+
+    function set(Key, MaybeValue) {
+
+        flocksLog(' - Flocks2 multi-set');
+
+        if      (typeof Key === 'string') { setByKey(Key, MaybeValue); }
+        else if (isArray(Key))            { setByPath(Key, MaybeValue); }
+        else if (isNonArrayObject(Key))   { setByObject(Key); }
+        else                              { throw 'Flocks2 set/1,2 key must be a string or an array'; }
+
+    }
+
+
+
+
+
+    function clone(obj, loglabel) {
+
+        flocksLog( ' + Flocks2 cloning ' + (loglabel? loglabel : JSON.stringify(obj).substring(0,100) ) );
 
         if ((null === obj) || ('object' != typeof obj)) { return obj; }
 
@@ -38,305 +139,123 @@ var clone = function(obj) {
 
         return copy;
 
-    }, // oh, javascript :|
+    }
 
 
 
 
 
-    flContextTypes = {
-        flocks_updater : React.PropTypes.object,
-        flocks_context : React.PropTypes.object
-    },
+    function attemptUpdate() {
+
+        flocksLog(' - Flocks2 attempting update');
+        dirty = true;
+
+        if (!(initialized)) {
+            flocksLog(' x Flocks2 skipped update: root is not initialized');
+            return null;
+        }
+
+        if (updateBlocks) {
+            flocksLog(' x Flocks2 skipped update: lock count updateBlocks is non-zero');
+            return null;
+        }
+/*
+        if (deepCompare(nextFCtx, prevFCtx)) {
+            flocksLog(' x Flocks2 skipped update: no update to state');
+            return true;
+        }
+*/
+        if (!(handler(nextFCtx))) {
+            flocksLog('   ! Flocks2 rolling back update: handler rejected propset');
+            nextFCtx = prevFCtx;
+            dirty    = false;
+            return null;
+        }
+
+        prevFCtx = nextFCtx;
+
+        flocksLog('   - Flocks2 update passed');
+        React.render( React.createFactory(tagtype)( { flocks2context: nextFCtx } ), document.body );
+        dirty = false;
+
+        flocksLog('   - Flocks2 update complete; finalizing');
+        finalizer();
+        return true;
+
+    }
 
 
 
 
 
-    /**
-     * @class FlocksMixin
-     * @since 0.4.0
-     */
+    function create(Options) {
 
-    Mixin = {
+        var FlocksConfig = Options.flocks_config || {},
+            FlocksData   = Options.flocks_data   || {},
+            InitialProps = Options.initial_props || {},
 
-        contextTypes      : flContextTypes,
-        childContextTypes : flContextTypes,
+            target       = FlocksConfig.target || document.body,
+            stub         = function() { window.alert('whargarbl stub'); attemptUpdate(); },
+            updater      = { get: stub, set: set, override: stub, clear: stub, update: stub, lock: stub, unlock: stub };
+
+        tagtype          = FlocksConfig.control,
+        nextFCtx         = FlocksData;
+
+        flocksLog('Flocks2 root creation begins');
+
+        if (!(tagtype))             { throw 'Flocks2 fatal error: must provide a control in create/2 FlocksConfig'; }
+        if (FlocksConfig.handler)   { handler   = FlocksConfig.handler;   flocksLog(' - Flocks2 handler assigned'  ); }
+        if (FlocksConfig.finalizer) { finalizer = FlocksConfig.finalizer; flocksLog(' - Flocks2 finalizer assigned'); }
+
+        if (FlocksConfig.preventAutoContext) {
+            flocksLog(' - Flocks2 skipping auto-context');
+        } else {
+            flocksLog(' - Flocks2 engaging auto-context');
+            this.fctx = clone(nextFCtx);
+        }
+
+        flocksLog('Flocks2 creation finished; initializing');
+        initialized = true;
+        attemptUpdate();
+
+        flocksLog('Flocks2 expose updater');
+        this.fupd = updater;
+        this.fset = updater.set;
+
+        flocksLog('Flocks2 initialization finished');
+
+        return updater;
+
+    }
+
+
+
+
+
+    var Mixin = {
+
+        contextTypes      : flocks2_ctxs,
+        childContextTypes : flocks2_ctxs,
 
         componentWillMount: function() {
-            this.fctx = function() { return clone(this.context.flocks_context); };
-            this.fset = function() { return this.context.flocks_updater.set; };
-            this.fupd = function() { return this.context.flocks_updater; };
+
+            flocksLog(' - Flocks2 component will mount: ' + this.constructor.displayName);
+            flocksLog(typeof this.props.flocks2context   === 'undefined'? '   - No F2 Context Prop' : '   - F2 Context Prop found');
+            flocksLog(typeof this.context.flocks2context === 'undefined'? '   - No F2 Context'      : '   - F2 Context found');
+
+            if (this.props.flocks2context) {
+                this.context.flocks2context = this.props.flocks2context;
+            }
+
+            this.fset = function(X,Y) { set(X,Y); };
+            this.fctx = this.context.flocks2context;
+
         },
 
         getChildContext: function() {
-
-            var defaultingContext = {};
-
-            // updater in props overrides contexts
-            if (typeof this.props.flocks_context !== 'undefined') {
-                defaultingContext.flocks_context = this.props.flocks_context;
-                delete defaultingContext.flocks_context.flocks_context;
-            }
-
-            if (typeof this.props.flocks_updater !== 'undefined') {
-                defaultingContext.flocks_updater = this.props.flocks_updater;
-            }
-
-            return defaultingContext;
-
+            return this.context;
         }
-    },
 
-
-
-
-
-    /**
-     * @method isArray
-     * @for    Flocks
-     * @return {Boolean} Whether or not the argument is an `Array`
-     */
-
-    isArray = function(maybeArray) {
-        return (Object.prototype.toString.call(maybeArray) === '[object Array]');
-    },
-
-
-
-
-
-    /**
-     * @method isNonArrayObject
-     * @for    Flocks
-     * @return {Boolean} Whether or not the argument is an `Object`, but also not an `Array`
-     */
-
-    isNonArrayObject = function(maybeArray) {
-        if (typeof maybeArray !== 'object')                                  { return false; }
-        if (Object.prototype.toString.call(maybeArray) === '[object Array]') { return false; }
-        return true;
-    },
-
-
-
-
-
-    /**
-     * @method enforceString
-     * @for    Flocks
-     * @return {undefined} Throw if the first argument isn't a `String`; otherwise return
-     */
-
-    enforceString = function(On, Label) {
-        Label = Label || 'Argument must be a string';
-        if (typeof On !== 'string') {
-            throw Label;
-        }
-    },
-
-
-
-
-
-    enforceArray = function(On, Label) {
-        Label = Label || 'Argument must be an array';
-        if (!(isArray(On))) {
-            throw Label;
-        }
-    },
-
-
-
-
-
-    enforceNonArrayObject = function(On, NonObjLabel, ArrayLabel) {
-        NonObjLabel = NonObjLabel || 'Argument must be a non-array object';
-        ArrayLabel  = ArrayLabel  || 'Argument must be a non-array object';
-        if (typeof On !== 'object') {
-            throw NonObjLabel;
-        }
-        if (isArray(On)) {
-            throw ( ArrayLabel || NonObjLabel );
-        }
-    },
-
-
-
-
-
-    create = function(Options) {
-
-        var currentData      = {},
-            prevData         = {},
-            updatesBlocked   = 0,
-            dirty            = false,
-
-            handler          = Options.before || function(C,P) { return true; },
-            finalizer        = Options.after  || function(C,P) { return null; },
-            TargetTag        = Options.target,
-            RenderDescriptor = Options.control,
-
-            updateIfWanted = function() {
-
-                if (updatesBlocked) {
-                    dirty = true;
-                    return null;
-                } else {
-
-                    if (!(handler(currentData, prevData))) {
-                        currentData = prevData;
-                        return false;
-                    }
-
-                    var cdata                = clone(currentData);
-                        cdata.flocks_context = currentData;
-
-                    finalizer(currentData, prevData);
-
-                    prevData             = clone(currentData);
-
-                    React.renderComponent(RenderDescriptor(cdata), TargetTag);
-                    dirty                = false;
-                    return true;
-
-                }
-            },
-
-            pathDive = function(CurPath, CurTgt) {
-                var parent = CurTgt;
-
-                for (var i = 0; i < CurPath.length-1; i += 1) {
-                    parent = parent[CurPath[i]];
-                }
-
-                return parent[CurPath[CurPath.length-1]];
-            },
-
-            pathSet = function(CurPath, Val) {
-
-                var parent = currentData;
-
-                if (CurPath.length === 1) {
-                    parent[CurPath[0]] = Val;
-                } else {
-                    for (var i = 0; i < CurPath.length-1; i += 1) {
-                        parent = parent[CurPath[i]];
-                    }
-                }
-
-                parent[CurPath[CurPath.length-1]] = Val;
-                return updateIfWanted();
-            },
-
-            setByObject = function(NaObject) {
-                enforceNonArrayObject(NaObject, 'Flocks.bulk_set takes an object', 'Flocks.bulk_set takes a non-array object');
-                if (handler(NaObject)) {
-                    currentData = NaObject;
-                }
-                return updateIfWanted();
-            },
-
-            setByPath = function(Path, Value) {
-                enforceArray(Path, 'Flock.path_set must take an array for its path');
-                return pathSet(Path, Value);
-            },
-
-            setByKey = function(Key, Value) {
-                enforceString(Key, 'Flock.member_set must take a string for its key');
-
-                var toHandle  = {};     // ... damnit json :|
-                toHandle[Key] = Value;
-
-                if (handler(toHandle)) {
-                    currentData[Key] = Value;
-                }
-
-                return updateIfWanted();
-            };
-
-        if (typeof TargetTag        === 'undefined') { throw 'flocks fatal error: must set a target'; }
-        if (typeof RenderDescriptor === 'undefined') { throw 'flocks fatal error: must set a control'; }
-
-
-
-
-
-        /**
-         * @class Flocks
-         * @since 0.1.1
-         */
-
-        return {
-
-            // need to honor Handler
-            // need set_path and get_path
-            // need announce
-
-            member_set: setByKey,
-
-            set: function(Key, MaybeValue) {
-
-                if      (typeof Key === 'string') { setByKey(Key, MaybeValue); }
-                else if (isArray(Key))            { setByPath(Key, MaybeValue); }
-                else if (isNonArrayObject(Key))   { setByObject(Key); }
-                else                              { throw 'Flocks.set/2 key must be a string or an array'; }
-
-            },
-
-            init: function(InitObj) {
-                InitObj.flocks_context = clone(InitObj);
-                this.set(InitObj);
-                this.set('flocks_updater', this);
-            },
-
-            // get isn't subject to handling
-            // todo whargarbl need path version
-            // todo whargarbl need argument type guards
-            get: function(What) {
-                return (What === undefined)? currentData : currentData[What];
-            },
-
-            bulk_set: setByObject,
-
-            bulk_update: function(request) {
-                enforceNonArrayObject(request, 'Flocks.bulk_update takes an object', 'Flocks.bulk_update takes a non-array object');
-
-                if (handler(request)) {
-                    var currentBase = currentData;
-                    for (var i in request.keys()) {
-                        currentBase[i] = request[i];
-                    }
-
-                    if (handler(currentBase)) {
-                        currentData = currentBase;
-                    }
-                }
-
-                return updateIfWanted();
-
-            },
-
-            clear: function() {
-                if (handler({})) {
-                    currentData = {};
-                }
-                return updateIfWanted();
-            },
-
-            // lock and unlock aren't subject to handling
-            lock: function() {
-                updatesBlocked += 1;
-                return true;
-            },
-
-            // lock and unlock aren't subject to handling
-            unlock: function() {
-                updatesBlocked -= 1;
-                return (updatesBlocked > 0)? updateIfWanted() : false;
-            }
-
-        };
 
     };
 
@@ -344,25 +263,34 @@ var clone = function(obj) {
 
 
 
-var exports = {
+    var exports = {
 
-    member                : Mixin,
-    create                : create,
-    clone                 : clone,
-    isArray               : isArray,
-    isNonArrayObject      : isNonArrayObject,
-    enforceString         : enforceString,
-    enforceArray          : enforceArray,
-    enforceNonArrayObject : enforceNonArrayObject
+        member                : Mixin,
+        create                : create,
+        clone                 : clone,
 
-};
+        isArray               : isArray,
+        isNonArrayObject      : isNonArrayObject,
+
+        enforceString         : enforceString,
+/*
+        enforceArray          : enforceArray,
+        enforceNonArrayObject : enforceNonArrayObject
+*/
+    };
 
 
 
 
 
-if (typeof module !== 'undefined') {
-    module.exports = exports;
-} else {
-    window.flocksjs = exports;
-}
+    if (typeof module !== 'undefined') {
+        module.exports = exports;
+    } else {
+        window.flocksjs2 = exports;
+    }
+
+
+
+
+
+}());
